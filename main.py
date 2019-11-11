44f13e0e0d942cda6b3164206da5b468b7c97448
Orlando Hernandez <oaraz02@gmail.com>
2019-11-10 21:05:57 -0500

    add activate functions

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 import random
 import matplotlib.pyplot as plt
 
@@ -6,7 +7,7 @@ import matplotlib.pyplot as plt
 class NeuralNetwork():
     def __init__(self, dataset_name='iris2Clas.csv', n_hidden_neurons=3, eta=0.35, itera=15000, percentage_train_dataset=0.8, id_activate_function=1):
         self.dataset = np.genfromtxt(dataset_name, delimiter=',')
-        self.dataset = self.dataset[1:]
+        self.dataset = self.normalization(self.dataset[1:])
         self.len_dataset = len(self.dataset)
         self.percentage_train_dataset = percentage_train_dataset
         self.split_dataset()
@@ -16,7 +17,7 @@ class NeuralNetwork():
         self.activation_functions = [
             {
                 'fn': lambda x: x,
-                'dfn': lambda _: 1
+                'dfn': lambda x: np.ones((len(x), 1))
             },
 
             {
@@ -26,15 +27,15 @@ class NeuralNetwork():
 
             {
                 'fn': lambda x: 2/(1 + np.exp(-x)) - 1,
-                'dfn': lambda x: 2*np.exp(-x)/((1 + np.exp(-x)) ^ 2)
+                'dfn': lambda x: 2*np.exp(-x)/((1 + np.exp(-x)) ** 2)
             },
 
             {
-                'fn': lambda x: 1 if x >= 0 else 0,
-                'dfn': lambda _: 0
+                'fn': lambda x: np.array([[(1 if v >= 0 else 0) for v in i] for i in x]),
+                'dfn': lambda x: np.zeros((len(x), 1))
             }
         ]
-        self.activate_function = self.activation_functions[id_activate_function]
+        self.activate_function = self.activation_functions[3]
         self.init_weights()
 
     def split_dataset(self):
@@ -68,18 +69,35 @@ class NeuralNetwork():
         # 1 because there 1 output
         self.syn1 = 2 * np.random.rand(self.n_hidden_neurons, 1) - 1
 
+    def normalization(self, dataset):
+        minimum_values = []
+        maximum_values = []
+        normalized = []
+        for i in range(len(dataset[0])):
+            # saca el minimo de todos los datos de cada columna
+            minimum_values.append(min(dataset[:, i]))
+            maximum_values.append(max(dataset[:, i]))
+
+        n = lambda x: [ (x[y] - minimum_values[y]) / (maximum_values[y] - minimum_values[y]) for y in range(len(x))] # funcion min-max
+        
+        for i in range(len(dataset)):
+            normalized.append(n(dataset[i]))
+        normalized = np.array(normalized)
+        return normalized
+
     def train(self):
         error_promedio = []
         error_promedio_test = []
         epoca = []
+        input_data = self.train_dataset
         for itera in range(self.itera):
-            input_data = self.train_dataset
             neta1 = np.dot(input_data, self.syn0)
-            print(f'Netas Intermedias: \n, {neta1 }')
+            # print(f'Netas Intermedias: \n, {neta1 }')
             l1 = self.activate_function['fn'](neta1)
             print(f'Salidas Intermedias: \n { l1 }')
 
             neta2 = np.dot(l1, self.syn1)
+
             print(f'Netas Salidas: \n { neta2 }')
             l2 = self.activate_function['fn'](neta2)
             print(f'Salida red: \n { l2 }')
@@ -100,7 +118,6 @@ class NeuralNetwork():
             # NO multiply how much we missed by the
             # NO slope of the sigmoid at the values in l1
             l2_delta = l2_error * self.activate_function['dfn'](l2)*self.eta
-
             print(f'l2_delta: \n {l2_delta} \n')
 
             # how much did each l1 value contribute to the l2 error
@@ -112,7 +129,9 @@ class NeuralNetwork():
             # in what direction is the target l1?
             # were we really sure? if so, don't change too much.
             l1_delta = l1_error * self.activate_function['dfn'](l1)*self.eta
+
             print(f'l1_delta: \n {l1_delta} \n')
+            print(f'l1_error: \n {l1_error} \n')
 
             # datos de TEST
             neta_test = np.dot(self.test_dataset, self.syn0)
@@ -122,8 +141,12 @@ class NeuralNetwork():
             error_test = self.y_test_dataset - output_test
             error_promedio_test.append(np.mean(np.abs(error_test)))
             # update weights
+            print(f' weights 0 before: \n { self.syn0 }')
+            print(f' weight 1 before : \n { self.syn1 }')
             self.syn1 += l1.T.dot(l2_delta)
             self.syn0 += input_data.T.dot(l1_delta)
+            print(f' weights 0 after: \n { self.syn0 }')
+            print(f' weight 1 after : \n { self.syn1 }')
 
         print('\n Output After Training:')
         print(f'Salida red: \n, {l2}')
@@ -145,19 +168,27 @@ class NeuralNetwork():
 
 
 def read_parameters():
-    dt = input('ingrese el nombre del dataset (default: iris2Clas.csv)')
-    eta = input('Ingrese el valor del del eta (default: 0.35)')
-    iteraciones = input('Ingrese el número de iteraciones (default: 15000)')
-    n_neurons = input(
-        'Ingrese el número de neuronas en la capa oculta (default: 3)')
+    dt = input('Ingrese el nombre del dataset (default: iris2Clas.csv)')
+    function_id = input('Ingrese la función que desea utilizar \n 0:Identidad \n 1:Sigmoide \n 2: \n 3: (default Sigmoide)')
+    eta = input('Ingrese el valor del del eta (default: 0.001 para función identidad, 0.35 en otro caso)')
+    iteraciones = input('Ingrese el número de épocas (default: 15000)')
+    n_neurons = input('Ingrese el número de neuronas en la capa oculta (default: 3)')
+    
 
     if not dt.strip():
         dt = 'iris2Clas.csv'
 
+    if not function_id.strip():
+        function_id = 1
+    else:
+        function_id = int(function_id)
+
     if not eta.strip():
-        eta = 0.35
+        eta = 0.35 if function_id else 0.001
     else:
         eta = float(eta)
+        if not function_id and eta > 0.001:
+            print("ETA demasiado alto para usar la función identidad como función de activación, esto llevará a nan")
 
     if not n_neurons.strip():
         n_neurons = 3
@@ -169,10 +200,10 @@ def read_parameters():
     else:
         iteraciones = int(iteraciones)
 
-    return dt, eta, iteraciones, n_neurons
+    return dt, eta, iteraciones, n_neurons, function_id
 
 
-dt, eta, itera, n_neurons = read_parameters()
+dt, eta, itera, n_neurons, funcion = read_parameters()
 s = NeuralNetwork(dataset_name=dt, eta=eta, itera=itera,
-                  n_hidden_neurons=n_neurons)
+                  n_hidden_neurons=n_neurons, id_activate_function=funcion)
 s.train()
